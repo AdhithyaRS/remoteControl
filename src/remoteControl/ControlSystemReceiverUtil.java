e04895694cf6be1800346ef6daab766cab8537b0
adhithyars <toadhi1509@gmail.com>
2023-06-22 18:48:44 +0530

    Update

--- src/remoteControl/ControlSystemReceiverUtil.java
+++ src/remoteControl/ControlSystemReceiverUtil.java
@@ -6,7 +6,9 @@ public class ControlSystemReceiverUtil {
 	
 	public static CentralReceiver getUnderlyingCommand(){
 		System.out.println("Please enter a command");
-		 String object = new Scanner(System.in).nextLine().toLowerCase(); 
+		Scanner sc = new Scanner(System.in);
+		 String object = sc.nextLine().toLowerCase(); 
+		 sc.close();
 		 System.out.println("The Command you want to execute is :"+object);
 		 if(object.contains("on") || object.contains("off")){
 			 return new SimpleSystemReceiver(object);
@@ -18,6 +20,8 @@ public class ControlSystemReceiverUtil {
 			 return new EnhancementSystemReceiver(object);
 		 }
 		 
+		 
 	}
+	
 	
 }
